6e356764afce0e6e6b5c76ff1893c624e987b708
Dejan <imdejan00@gmail.com>
2022-08-24 15:46:39 +0200

    Added select options on pricing

--- Pricing_1/app.js
+++ Pricing_1/app.js
@@ -2,10 +2,22 @@
 let MA=document.querySelectorAll(".padding-time")
 let tw= document.querySelector(".time-wrap")
 
+let cardContainer= document.querySelector(".card-container")
+let cards= document.querySelectorAll(".card")
+
 tw.addEventListener("click", e=>{
     MA.forEach((el, inx, arr)=>{
         el.classList.toggle("isActive")
     })
+})
+
+
+cards.forEach((el, indx, arr)=>{
+    el.addEventListener("click", e=>{
+        Array.from(cards).filter(i=>i!=el).forEach(el=>el.classList.remove("isSelected"))
+        el.classList.toggle("isSelected")
+        }
+    )
 })
 
 
