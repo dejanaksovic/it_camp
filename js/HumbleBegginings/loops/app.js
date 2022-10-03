837ea063777d4efde91da396c18413d9ea3686d4
Dejan <imdejan00@gmail.com>
2022-10-03 17:56:03 +0200

    For loops, nit-commit

---
+++ js/HumbleBegginings/loops/app.js
@@ -0,0 +1,32 @@
+//Basic for loop
+//
+// for (let i=0 ; i<10; i++) {
+//     console.log(`${i**2}`)
+// }
+
+
+//Excercise 1: write down numbers in reverse order from '1' to '10'
+//
+// for (let i=6; i>0; i--) {
+//     console.log(`${i}`)
+// }
+
+//Excercise 2: write down numbers from '1' to '10' excluding '7'
+//
+// for (let i=1; i<11; i++) {
+//     if(i===7)
+//     continue
+//     console.log(i)
+// }
+
+//Exercise 3: Write a program that writes down a sum of all even numbers from '1' to '20'
+//
+// let add=0;
+// for (let i=0; i<=20; i+=2) {
+//     console.log("Nakon broja", i, "Zbir je", add+=i)
+    //Extra
+    // if(!(i%2)) {
+    //     add+=i
+    //     console.log("Nakon broja ", i, "Zbir je", add)
+    // }
+// }
